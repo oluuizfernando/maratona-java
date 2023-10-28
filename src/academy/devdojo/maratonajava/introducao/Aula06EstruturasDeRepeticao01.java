2e9ede99c8e962336a79295f558f8fa6558dab24
oluuizfernando <104778712+oluuizfernando@users.noreply.github.com>
2023-10-28 15:20:34 -0300

    Estruturas de Repeticao

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao01 {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;++i){
+            System.out.println(i);
+        }
+    }
+}
