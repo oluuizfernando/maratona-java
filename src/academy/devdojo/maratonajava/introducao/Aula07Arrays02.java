fb956cee012e06244d0dd7eb9936203f8320834b
oluuizfernando <104778712+oluuizfernando@users.noreply.github.com>
2023-10-29 20:51:59 -0300

    Continuaçao de Arrays

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        int[] numeros = {1,2,3,4,5};
+        int[] numeros2 = new int[]{1,2,3,4,5};
+
+        //foreach
+
+        for (int num: numeros2) {
+            System.out.println(num);
+        }
+    }
+}
